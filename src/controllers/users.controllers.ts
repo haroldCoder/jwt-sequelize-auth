8e5f34e41863e3a0f2f5ea2e65cef276a7ea1b21
haroldCoder <haroldc2005@gmail.com>
2024-01-01 12:56:09 -0500

    create user & implement in middleware

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,20 +1,38 @@
 import { Request, Response } from "express";
 import UserModel from "../models/users.model";
 import { Userauth } from "../interface/userauth";
+import { json } from "body-parser";
 
-class UsersControllers extends UserModel{
+class UsersControllers extends UserModel {
     req: Request
     res: Response
 
-    constructor(req: Request, res: Response){
+    constructor(req: Request, res: Response) {
         super();
         this.req = req;
         this.res = res;
     }
 
-    getUsers = async() =>{
+    getUsers = async () => {
         const users: Array<Userauth> = await this.model_user.findAll();
         this.res.status(200).json(users);
+    }
+
+    createUser = async (user: Userauth) => {
+        try {
+            await this.model_user.create({
+                username: user.username,
+                email: user.email,
+                password: user.password,
+                token: user.password,
+                date: user.date
+            });
+        }
+        catch(err){
+            console.log(err);
+            this.res.status(500).json(err);
+            throw err;
+        }
     }
 }
 
--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,45 +1,59 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { Userauth } from "../interface/userauth";
+import UsersControllers from "../controllers/users.controllers";
 
 
 class Auth {
-    Register = (req: Request, res: Response, next: NextFunction) =>{
-        try{
-           const {data}: {data: Userauth} = req.body;
+    Register = (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const data: Userauth = req.body;
+            console.log(data);
+
 
             const token = jwt.sign({
                 username: data.username,
                 email: data.email,
                 password: data.password,
-                token: data.token,
                 date: data.date || new Date()
             }, data.password);
 
-            res.status(200).json({
-                user: data.username,
+            new UsersControllers(req, res).createUser({
+                username: data.username,
+                email: data.email,
+                password: data.password,
+                date: data.date || new Date(),
                 token: token
-            }) 
+            }).then(() => {
+                res.status(200).json({
+                    user: data.username,
+                    token: token
+                })
+            })
+
+
         }
-        catch(err){
+        catch (err) {
             res.status(500).send(err);
+            console.log(err);
+
         }
     }
 
-    Login = (req: Request, res: Response, next: NextFunction) =>{
-        try{
-            const {auth, password} = req.query;
+    Login = (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const { auth, password } = req.query;
 
             let token = jwt.verify(typeof auth === "string" ? auth : "", typeof password === "string" ? password : "");
 
-            if(token){
+            if (token) {
                 next();
             }
-            else{
+            else {
                 res.status(500).send(`token or password invalid`);
             }
         }
-        catch(err){
+        catch (err) {
             res.status(500).send(err);
         }
     }
