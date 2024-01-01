0425e62b7b44ba760720a4ddb63ae76c74169297
haroldCoder <haroldc2005@gmail.com>
2024-01-01 12:31:00 -0500

    remove sigleton pattern

--- src/connection/sequelizeConnect.ts
+++ src/connection/sequelizeConnect.ts
@@ -1,28 +1,23 @@
-import {Sequelize, Dialect} from "sequelize";
+import { Sequelize, Dialect } from "sequelize";
 
-class SequelizeConnect{
+class SequelizeConnect {
     sqlize: Sequelize;
-    static User: SequelizeConnect
 
-    constructor(database: string, username: string, password: string, host: string, db: Dialect){
+    constructor(database: string, username: string, password: string, host: string, db: Dialect) {
         this.sqlize = new Sequelize(database, username, password, {
             host: host,
             dialect: db
         })
-        if(SequelizeConnect.User){
-            throw new Error("You can only create one instance!")
-        }
-        SequelizeConnect.User = this;   
     }
 
-    isConnect = async() =>{
-        try{
+    isConnect = async () => {
+        try {
             await this.sqlize.authenticate();
             console.log('Connection has been established successfully');
         }
-        catch(err){
+        catch (err) {
             console.log('Unable to connect to the database: ', err);
-            
+
         }
     }
 }
