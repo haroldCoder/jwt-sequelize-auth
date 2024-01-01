1fa9b5ab7fc3803481a07817216b586bbca7644b
haroldCoder <haroldc2005@gmail.com>
2024-01-01 11:07:22 -0500

    fix name of environment variables password and host

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -5,7 +5,7 @@ class UserModel extends SequelizeConnect{
     model_user: ModelCtor<any>;
 
     constructor(){
-        super(process.env.MYSQL_HOST!, process.env.MYSQL_USER!, process.env.PASSWORD!, process.env.HOST!, "mysql");
+        super(process.env.MYSQL_DB!, process.env.MYSQL_USER!, process.env.MYSQL_PASSWORD!, process.env.MYSQL_HOST!, "mysql");
         this.model_user = this.sqlize.define('usersdb', {
             Id: {
                 type: DataTypes.INTEGER,
