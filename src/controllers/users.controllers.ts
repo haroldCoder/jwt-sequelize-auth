4401bb241f8df1b73d4e0b316a6ed69c5dc24046
haroldCoder <haroldc2005@gmail.com>
2024-01-06 14:17:11 -0500

    create updateuser & deleteuser services & add jest and mocha

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import UserModel from "../models/users.model";
 import { Userauth } from "../interface/userauth";
-import { json } from "body-parser";
 
 class UsersControllers extends UserModel {
     req: Request
@@ -29,6 +28,47 @@ class UsersControllers extends UserModel {
                 token: user.token,
                 date: user.date
             });
+
+            this.res.status(200).send(`${user.username} is added`);
+        }
+        catch(err){
+            console.log(err);
+            this.res.status(500).json(err);
+            throw err;
+        }
+    }
+
+    deleteUser = async(Id: number) =>{
+        try{
+            await this.model_user.destroy({
+                where: {
+                    Id
+                },
+                force: true
+            })
+
+            this.res.status(200).send("user is removed")
+        }
+        catch(err){
+            console.log(err);
+            this.res.status(500).json(err);
+            throw err;
+        }
+    }
+
+    updateUser = async({username, email, password, date}: Userauth, id: number) =>{
+        if(id) throw new Error("This service need Id");
+        try{
+            await this.model_user.update({
+                username: username,
+                email: email,
+                password: password,
+                date: date
+            },{where: {
+                Id: id
+            }})
+
+            this.res.status(200).send("user is updated")
         }
         catch(err){
             console.log(err);
--- src/routes/usersauth.route.ts
+++ src/routes/usersauth.route.ts
@@ -5,6 +5,8 @@ const router = Router();
 router.route("/users")
 .get((req: Request, res: Response)=>new UsersControllers(req, res).getUsers())
 
-
+router.route("/users/:id")
+.delete((req: Request, res: Response)=>new UsersControllers(req, res).deleteUser(parseInt(req.params.id)))
+.put((req: Request, res: Response)=>new UsersControllers(req, res).updateUser(req.body, parseInt(req.params.Id)));
 
 module.exports = router;
